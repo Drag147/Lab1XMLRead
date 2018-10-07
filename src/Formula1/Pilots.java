e7ad640d39eb9c52c94310079ade20a1a15aaaf2
Sergey <sir.ser20@yandex.ru>
2018-10-07 04:10:29 +0300

    Add class Pilots

--- src/Formula1/Pilots.java
+++ src/Formula1/Pilots.java
@@ -1,5 +1,57 @@
 package Formula1;
 
+import java.util.Date;
+
 public class Pilots {
-    private byte Personal_pilot_number;
+    private short PersonalPilotNumber;
+    private String Name;
+    private String Surname;
+    private Date DateBirthDay;
+
+    public Pilots()
+    {
+        Name = "";
+        Surname = "";
+        DateBirthDay = new Date();
+    }
+
+    public Pilots(short PersonalPilotNumber, String Name, String Surname, Date DateBirthDay)
+    {
+        this.PersonalPilotNumber = PersonalPilotNumber;
+        this.Name = Name;
+        this.Surname = Surname;
+        this.DateBirthDay = DateBirthDay;
+    }
+
+    public short getPersonalPilotNumber() {
+        return PersonalPilotNumber;
+    }
+
+    public void setPersonalPilotNumber(byte personalPilotNumber) {
+        PersonalPilotNumber = personalPilotNumber;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String surname) {
+        Surname = surname;
+    }
+
+    public Date getDateBirthDay() {
+        return DateBirthDay;
+    }
+
+    public void setDateBirthDay(Date dateBirthDay) {
+        DateBirthDay = dateBirthDay;
+    }
 }
